e264892f564749059555feaa0c77ab4ab527d4b0
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-09-16 10:10:32 +0700

    CRUD Basic

--- src/main/java/com/se/springbootapi/controller/EmployeeController.java
+++ src/main/java/com/se/springbootapi/controller/EmployeeController.java
@@ -1,7 +1,38 @@
 package com.se.springbootapi.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.se.springbootapi.entity.Employee;
+import com.se.springbootapi.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable("id") Long employeeId){
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployeeById(@PathVariable("id") Long employeeId, @RequestBody Employee employee){
+        return employeeService.updateEmployeeById(employeeId, employee);
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteById(@PathVariable("id") Long employeeId){
+        employeeService.deleteById(employeeId);
+        return "Thanh Cong";
+    }
 }
--- src/main/java/com/se/springbootapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/se/springbootapi/service/EmployeeServiceImpl.java
@@ -1,34 +1,45 @@
 package com.se.springbootapi.service;
 
 import com.se.springbootapi.entity.Employee;
+import com.se.springbootapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Employee getEmployeeById(Long employeeId) {
-        return null;
+        return employeeRepository.findById(employeeId).get();
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee saveEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public Employee updateEmployeeById(Long employeeId, Employee employee) {
-        return null;
+        Employee employeeDB = employeeRepository.findById(employeeId).get();
+        employeeDB.setFirstName(employee.getLastName());
+        employeeDB.setLastName(employee.getLastName());
+        employeeDB.setEmail(employee.getEmail());
+        return employeeRepository.save(employeeDB);
     }
 
     @Override
     public void deleteById(Long employeeId) {
-
+        Employee employee = employeeRepository.findById(employeeId).get();
+        employeeRepository.delete(employee);
     }
 }
